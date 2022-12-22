2a79a5e45ea8c623c0c066754e9472833e4ae166
Ngeyen Victory <ngeyennv@gmail.com>
2022-12-22 16:11:43 +0200

    creating notes urls

--- notes/urls.py
+++ notes/urls.py
@@ -1,1 +1,15 @@
+from notes.models import Note
+from notes.serializers import NoteSerialzer
+from rest_framework import generics
+from notes import views
+from django.urls import re_path
+
+urlpatterns = [
+    re_path(r"^$", views.ListAllNotes.as_view(), name="all-notes"),
+    re_path(r"^(?P<slug>[\w\-]+)/$",
+            views.RetrieveUpdateDelete.as_view(), name="get-note"),
+
+]
+
+
 # Create your views here.
