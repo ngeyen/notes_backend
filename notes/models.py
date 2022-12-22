f969d552d5bb4c85cef1fc9ded74b93ba9637296
Ngeyen Victory <ngeyennv@gmail.com>
2022-12-22 16:10:54 +0200

    creation of notes model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.db import models
+from django.utils.text import slugify
+import uuid
+from django.db.models.signals import pre_save
 
-# Create your tests here.
+# Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, )
+    date_updated = models.DateTimeField(auto_now=True,)
+
+
+def pre_save_note_receiver(sender, instance, **kwargs):
+    instance.slug = slugify(instance.title)
+
+
+pre_save.connect(pre_save_note_receiver, sender=Note)
