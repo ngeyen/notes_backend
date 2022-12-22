c241592290e45265442ce254f3f24bed2b5ab2fc
Ngeyen Victory <ngeyennv@gmail.com>
2022-12-22 16:11:55 +0200

    created notes views

--- notes/views.py
+++ notes/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from notes.serializers import NoteSerialzer
+from notes.models import Note
+# Create your views here.
+
+
+class ListAllNotes(generics.ListCreateAPIView):
+    serializer_class = NoteSerialzer
+    queryset = Note.objects.all()
+
+
+class RetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerialzer
+    queryset = Note.objects.all()
+    lookup_field = "slug"
